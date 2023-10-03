804dcb92f9f7def6b6cabf05293c7dfc7ace2376
Anurag Chauhan <114475545+Anurag123Chauhan@users.noreply.github.com>
2023-10-03 12:18:31 +0000

    commit

---
+++ column_name_from_a_given_column_number.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {   
+        vector<char>arr={ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int c=0;
+        string s;
+        if(n<=26){
+            s=arr[n-1];
+            return s;
+        }
+        else{
+            while(n>0){
+                c=(n-1)%26;
+                n=(n-1)/26;
+                s=arr[c]+s;
+            }
+            return s;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
